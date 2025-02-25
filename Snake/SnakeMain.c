2a51c8233c1585aa5bcd9d5eb8d1f7ba7827dad8
Evan Travis <Etravis9851@conestogac.on.ca>
2025-02-25 12:51:01 -0800

    - Fixed the ability to go straight backwards - Started developing the tail of the snake

--- Snake/SnakeMain.c
+++ Snake/SnakeMain.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <windows.h>
-#define WIDTH		21
-#define HEIGHT		10
+#define WIDTH		41
+#define HEIGHT		20
 
 int snakeTailX[100], snakeTailY[100];
 
 int x, y, gameOver, key, snakeLength;
 
 void initalizeGame() {
-
+	
 	gameOver = 0;
 
 	x = WIDTH / 2;
@@ -23,14 +23,15 @@ void setCursorPosition(int x, int y) { // this is a function made by windows to 
 void printGrid() {
 	setCursorPosition(0, 0); // this allows for the grid to replace itself rather than constantly print downward 
 
-	for (int i = 0; i < WIDTH; i++) { printf("#"); }
+	for (int i = 0; i < WIDTH / 2 + 1; i++) { printf("[]"); }
+	printf("]");
 	printf("\n");
 
 	for (int i = 0; i < HEIGHT; i++) {
 		for (int j = 0; j < WIDTH; j++) {
 
 			if (j == 0 || j == WIDTH - 1) {
-				printf("=");
+				printf("[]");
 			}
 
 			else if (j == x && i == y) {
@@ -53,7 +54,8 @@ void printGrid() {
 		printf("\n");
 	}
 
-	for (int i = 0; i < WIDTH; i++) { printf("#"); }
+	for (int i = 0; i < WIDTH / 2 + 1; i++) { printf("[]"); }
+	printf("]");
 	printf("\n");
 }
 
@@ -61,20 +63,20 @@ int input() { // accept inputs and set them to a key value
 	while (kbhit()) {
 		switch (tolower(getch())) {
 		case 'w':
-			if (key != 3);
-			key = 1;
+			if (key != 3)
+				key = 1;
 			break;
 		case 'a':
-			if (key != 4);
-			key = 2;
+			if (key != 4)
+				key = 2;
 			break;
 		case 's':
-			if (key != 1);
-			key = 3;
+			if (key != 1)
+				key = 3;
 			break;
 		case 'd':
-			if (key != 2);
-			key = 4;
+			if (key != 2)
+				key = 4;
 			break;
 		case 'x':
 			gameOver = 1;
@@ -89,19 +91,35 @@ void logic() {
 
 	switch (key) { // snake head movement 
 		case 1:  // w - up
-			Sleep(15); // slow down sleep of vertical movement
+			Sleep(30); // slow down sleep of vertical movement
 			y--; 
 			break;
 		case 2:  // a - left
 			x--;
 			break;
 		case 3:  // s - down
-			Sleep(15);
+			Sleep(30);
 			y++;
 			break;
 		case 4:  // d - right
 			x++;
 			break;
+	}
+
+	int prevX = snakeTailX[0]; // initailize the previous body sections
+	int prevY = snakeTailY[0];
+	int prev2X, prev2Y; // buffers
+
+	snakeTailX[0] = x; // set the tail to the tails to the current x, y from initalizeGame 
+	snakeTailY[0] = y;
+
+	for (int i = 1; i < snakeLength; i++) { // for everything (after the head (1)) till the total length of the snake
+		prev2X = snakeTailX[i]; 
+		prev2Y = snakeTailY[i];
+		snakeTailX[i] = prevX;
+		snakeTailY[i] = prevY;
+		prevX = prev2X;
+		prevY = prev2Y;
 	}
 
 	if (x < 0 || x >= WIDTH || y < 0 || y >= HEIGHT) { // gameover hitting walls
@@ -121,10 +139,10 @@ int main() {
 
 	while (!gameOver) {
 		
-		printGrid(); // find how to fix the blinking issue
-		input(); // fix the ability of going straight backward
+		printGrid(); 
+		input();
 		logic(); // continue developing this (specifically fruit)
-		Sleep(50);
+		Sleep(100);
 	}
 
 	return 0;
